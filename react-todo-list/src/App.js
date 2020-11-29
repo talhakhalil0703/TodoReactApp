b23617ebcede49734633309e82dd8435dcc1affb
talhakhalil0703 <tkhalil0703@gmail.com>
2020-11-29 04:10:11 -0700

    Added local storage

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,10 +1,24 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import './App.css';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
+const LOCAL_STORAGE_KEY = "react-todo-list-app-for-learning";
+
 function App() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(()=> {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos){
+      setTodos(storageTodos)
+    }
+  }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(todo){
     setTodos([todo, ...todos]);
@@ -13,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>React TODO</p>
+        <p>TODO List</p>
         <TodoForm addTodo={addTodo}/>
         <TodoList todos = {todos} /> 
       </header>
