60f13a9b4ee78a1949cb282a612998a513d3bd3e
Talha Khalil <talhakhalil@Talhas-MacBook-Pro.local>
2020-11-29 03:04:40 -0700

    added TodoForm

---
+++ react-todo-list/src/components/TodoForm.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+
+function TodoForm() {
+
+    const [todo, setTodo] = useState({
+        id: "",
+        task: "",
+        completed: false
+    });
+
+    function handleTaskInputChange(e){
+        setTodo({...todo, task: e.target.value});
+    }
+
+    return (
+        <form>
+            <input 
+            name = "task"
+            type = "text"
+            value={todo.task}
+            onChange={handleTaskInputChange}
+            />
+            <button type = "submit"/>
+        </form>
+    );
+}
+
+export default TodoForm;
